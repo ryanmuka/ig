2fe21979e0e7b91fcea8f9feedce628b3e39ca46
ryanmuka <37466116+ryanmuka@users.noreply.github.com>
2020-04-17 12:45:15 +0700

    Update unfollnotfollback.php

--- unfollnotfollback.php
+++ unfollnotfollback.php
@@ -1,79 +1,54 @@
 <?php
+//ikiganteng
 include 'class_ig.php';
 error_reporting(0);
-clear();
-echo "
-Ξ TITLE  : UNFOLLOW NOT FOLLOWBACK [Set Sleep]
-Ξ CODEBY : Firdy [https://fb.com/6null9]
-Ξ STATUS : Tools Function Properly [✓]
+date_default_timezone_set('Asia/Jakarta');
 
-";
+clear();
+copycat();
 $u = getUsername();
 $p = getPassword();
-$sleep = getComment('[?] Sleep in seconds: ');
-echo '•••••••••••••••••••••••••••••••••••••••••' . PHP_EOL . PHP_EOL;
+echo PHP_EOL;
+//$sleep = getComment('Sleep in seconds: ');
+echo '############################################################' . PHP_EOL . PHP_EOL;
 $login = login($u, $p);
-if ($login['status'] == 'success')
-{
-    echo color() ["LG"] . '[ * ] Login as ' . $login['username'] . ' Success!' . PHP_EOL;
-    $data_login = array(
-        'username' => $login['username'],
-        'csrftoken' => $login['csrftoken'],
-        'sessionid' => $login['sessionid']
-    );
-    $data_target = findProfile($u, $data_login);
-    echo color() ["LG"] . '[ # ] Name: ' . $data_target['fullname'] . ' | Followers: ' . $data_target['followers'] . ' | Following: ' . $data_target['following'] . PHP_EOL;
-    if ($data_target['status'] == 'success')
-    {
-        $cmt = 0;
-        for ($i = 1;$i < $data_target['following'];$i++)
-        {
-            $profile = getFollowing($u, $data_login, $i, 1);
-            foreach ($profile as $rs)
-            {
-                $username = $rs->username;
-                $post = findProfile($username, $data_login);
-                if ($post['is_polbek'] == 'true')
-                {
-                    echo color() ["LR"] . '[+] ' . $username . ' | Udh Follback' . PHP_EOL;
-                }
-                else
-                {
-                    echo color() ["LR"] . '[+] ' . $username . ' | Belom Follback ';
-                    $unfollow = unfollow($username, $data_login);
-                    if ($unfollow['status'] == 'success')
-                    {
+if ($login['status'] == 'success') {
+	echo color()["LG"].'[ * ] Login as '.$login['username'].' Success Login Sob xixi!' . PHP_EOL;
+	$data_login = array(
+		'username' => $login['username'],
+		'csrftoken'	=> $login['csrftoken'],
+		'sessionid'	=> $login['sessionid']
+	);
+	$data_target = findProfile($u, $data_login);
+	echo color()["LG"].'[ # ] Name: '.$data_target['fullname'].' | Followers: '.$data_target['followers'] .' | Following: '.$data_target['following'] . PHP_EOL;
+	if ($data_target['status'] == 'success') {
+			$profile = getFollowing($u , $data_login, $data_target['following'], 1);
+			foreach ($profile as $rs) {
+				$username = $rs->username;
+				$id_user = $rs->id;
+				$post = findProfile($username, $data_login);
+				if ($post['is_polbek'] == 'true') {
+					echo color()["CY"].'[+] '.date('H:i:s').' @'.$username.' | Udh Follback kok' .PHP_EOL;
+					sleep(1);
+				}else{
+				echo color()["LR"].'[-] '.date('H:i:s').' @'.$username.' | Belom Follback dia' ;
+				$unfollow = unfollows($id_user, $data_login);
+				if ($unfollow['status'] == 'success') {
+					echo color()["LG"].' (Unfollow @'.$username.' Success Unfollow Bro xixixi)';
+					sleep(rand(70,100));
+					echo PHP_EOL;
+				}else{
+					echo color()["LR"].'(Unfollow @'.$username.' Failed) Throttled! Resting during 10 minutes before try again.';
+					sleep(10*60);		
+					echo PHP_EOL;
+				}
+			}
+			}
 
-                        echo color() ["LG"] . '(Unfollow ' . $username . ' Success)';
-                        echo '[+] [' . date("H:i:s") . '] Tidur selama ' . $sleep . ' detik [+]' . PHP_EOL;
-                        sleep($sleep);
-                        echo PHP_EOL;
-                    }
-                    else
-                    {
-
-                        echo color() ["LR"] . '(Unfollow ' . $username . ' Failed)';
-                        echo '[+] [' . date("H:i:s") . '] Tidur selama ' . $sleep . ' detik [+]' . PHP_EOL;
-                        sleep($sleep);
-                        echo PHP_EOL;
-                    }
-
-                }
-            }
-
-        }
-
-    }
-    else
-    {
-
-        echo 'Error!';
-        echo PHP_EOL;
-    }
+	}else{
+		echo 'Error!';
+		echo PHP_EOL;
+	}
+}else{
+		echo color()['LR'].'Error: '.ucfirst($login['details'])."" . PHP_EOL;
 }
-else
-{
-
-    echo color() ["LR"] . '[ * ] Login as ' . $login['username'] . ' Failed!' . PHP_EOL;
-}
-
